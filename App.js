6420ed4d0ec99d8c5409268941464c7e2ffb13b7
Your Namemax-remacle <remacle.maxime@gmail.com>
2021-03-09 15:26:51 +1300

    improved functionality to fetch place name from geolocation

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useEffect} from 'react';
+import React,{useEffect,useState,} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
+import {getCityName} from './api'
 
 export default function App() {
 
+  const[location,setLocation] = useState(null)
+  const [city,setCity] = useState(null)
+  
+  
   useEffect(() =>{
     navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      ({coords}) => {
+        const lat = coords.latitude
+        const long = coords.longitude
+        setLocation(`${lat}, ${long}`)
+        console.log(location);
+      },
       (err) => console.log(err),
-      { enableHighAccuracy: true, timeout: 8000, maximumAge: 10000 }
+      { enableHighAccuracy: true, timeout: 8000, maximumAge: 2 }
     );
-  },[])
+    if(location != null){
+      getCityName(location)
+      .then(data =>{
+        setCity(data.results[0].locality)
+      })
+      .catch(err => console.log(err.message))
+    }
+  },[location])
+
+
 
   return (
     <View style={styles.container}>
-      <Text>ITS WORKING!!</Text>
+      <Text>{city}</Text>
       <StatusBar style="auto" />
     </View>
   );
